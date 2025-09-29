e80a4020ab9c585b69fe737711914339201d4262
NicolasdeZucca <zuccarellonj@gmail.com>
2025-09-28 23:34:15 -0300

    feat: initialize socket application with basic setup client-server

---
+++ models/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.port;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
+
+        //Middlewares
+        this.middlewares();
+
+        //Routes
+        this.routes();
+
+        //Sockets
+        this.sockets();
+    };
+
+    middlewares() {
+        //Cors
+        this.app.use(cors());
+
+        //public directory
+        this.app.use(express.static('public'));
+    };
+
+    sockets(){
+        this.io.on("connection", (socket) => {
+            console.log("Cliente conectado")
+        
+        
+            socket.on("disconnect", () => {
+                console.log("Cliente desconectado")
+            });
+        
+        });
+
+    };
+
+
+    routes() {
+    };
+
+    listen() {
+        this.server.listen(this.port, () => {
+            console.log(`Escuchando en el puerto: ${ this.port }`);
+        })
+    };
+
+};
+
+
+
+module.exports = Server;
